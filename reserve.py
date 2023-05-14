3e1ff9c85c8e61c3defd433f668224ed6f460442
ChaXxl <40326898+ChaXxl@users.noreply.github.com>
2023-05-14 22:47:45 +0800

    完善代码的注释

--- reserve.py
+++ reserve.py
@@ -5,10 +5,14 @@ from libs.info import infos
 from libs.source import ZWYT
 
 if __name__ == '__main__':
+    # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码
     for stu in infos:
         try:
+            # 初始化类示例，传入昵称、用户名、密码
             yy = ZWYT(stu['name'], stu['sno'], stu['pwd'])
-            yy.reserve(stu['devName'])  # 预约
+            
+            # 调用预约函数预约，传入预约座位号
+            yy.reserve(stu['devName'])  
         except Exception as e:
             print(e)
             continue
