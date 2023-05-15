f3fd39f8f4b2a79868997903db1cafa88750a0b9
ChaXxl <40326898+ChaXxl@users.noreply.github.com>
2023-05-15 10:23:49 +0800

    对 "该时间段有预约了" 的打印加入时间段信息

--- libs/source.py
+++ libs/source.py
@@ -275,7 +275,7 @@ class ZWYT(object):
             elif re.findall('当前时段有预约', message):
                 print(
                     "\033[0;33m" +
-                    {self.name}, 在这个时间段内已经有了预约: {json_data['resvBeginTime']} ~ {json_data['resvEndTime']} +
+                    f"{self.name}, 在这个时间段内已经有了预约: {json_data['resvBeginTime']} ~ {json_data['resvEndTime']}" +
                     "\033[0m"
                 )
               
