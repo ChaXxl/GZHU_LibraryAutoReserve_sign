f61c87ad2341fe4698a704647218545eefc09421
decike <879993212@qq.com>
2023-09-08 22:30:49 +0800

    工作流再测试

--- reserve.py
+++ reserve.py
@@ -5,6 +5,29 @@ from libs.info import infos
 from libs.source import ZWYT
 
 if __name__ == '__main__':
+    # action工作流
+    # 获取secrets里面的数据
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    devName = os.getenv('DEVNAME')
+    periods = os.getenv('PERIODS')
+
+    if username and password and devName and periods:
+        try:
+            # 初始化类示例，传入用户名、密码、时间段
+            yy = ZWYT('action', username, password, periods)
+            
+            # 调用预约函数预约，传入预约座位号
+            yy.reserve(devName)  
+        except Exception as e:
+            print(e)
+            continue
+
+
+
+
+
+    # 本地运行
     # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码、时间段
     for stu in infos:
         try:
